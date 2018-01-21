d2fac3110ef61d338406fcb1e8bdd689334f8a8a
Marcus Treu <marcus.d.treu@studium.uni-erlangen.de>
2018-01-22 00:05:44 +0100

    add startseite

--- www/index.php
+++ www/index.php
@@ -46,25 +46,7 @@ if (isset($_GET["section"])) {
 	</div>
 	
 	<!-- Content -->
-	<div class="content">
-		<div class="container">
-			<!-- Post -->
-			<div class="post">
-				<!-- Heading -->
-				<a href="#"><h1>Herzlich Willkommen In Pappenheim</h1></a>
-				<hr>
-				<div class="in-content">
-					<img class="right" src="img/post-img.jpg" alt="">
-					<p>
-					Wir vermieten eine gemütliche vier Sterne Komfort-Ferienwohnung im idyllisch gelegenen Luftkurort 
-					Pappenheim im Naturpark Altmühltal nahe dem fränkischen Seenland. Erholen Sie sich mitten in der Natur oder 
-					nehmen Sie an dem umfangreichen Freizeitangebot in und um Pappenheim teil.
-					</p>
-				</div>
-			</div>
-
-		</div>
-	</div>
+    	<?php include("sites.php")?>	
 
 	<!--
 	<footer>
---
+++ www/sites.php
@@ -0,0 +1,9 @@
+<?php
+   if($section == "wohnung"){include("wohnung.php");}
+   elseif($section == "freizeit"){include("freizeit.php");}
+   elseif($section == "anfahrt"){include("anfahrt.php");}
+   elseif($section == "preise"){include("preise.php");}
+   elseif($section == "buchen"){include("buchen.php");}
+   elseif($section == "kontakt"){include("kontakt.php");}
+   else{include("startseite.php");}
+?>
